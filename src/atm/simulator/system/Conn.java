2dabaa318beb69b8b0a1e31345cee195433a2b36
Codedevvv <vijaypatil0516@gmail.com>
2024-09-22 23:35:46 +0530

    Updated

--- src/atm/simulator/system/Conn.java
+++ src/atm/simulator/system/Conn.java
@@ -7,12 +7,15 @@ public class Conn {
     
     Connection c;
     Statement s;
+    String databaseName = "yourDatabaseName";
+    String user = "yourUserName";
+    String password = "yourPassword";
     
     public Conn(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            c = DriverManager.getConnection("jdbc:mysql:///atmsimulatorsystem", "root","admin");
+            c = DriverManager.getConnection("jdbc:mysql:///"+databaseName, user,password);
             s = c.createStatement();
             
         } catch(Exception e){
